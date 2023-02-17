HalfDrizzle/protobuf-no-gc

Test mac tweaks to fix flaky failures and lag
PiperOrigin-RevId: 508555693

src/google/protobuf/arena.h
@@ -110,6 +110,8 @@ void arena_delete_object(void* object) {
 }
 }  // namespace internal
 
+constexpr char* kNull = nullptr;
+
 // ArenaOptions provides optional additional parameters to arena construction
 // that control its block-allocation behavior.
 struct ArenaOptions {
@@ -127,7 +129,7 @@ struct ArenaOptions {
   // provided, the block must live at least as long as the arena itself. The
   // creator of the Arena retains ownership of the block after the Arena is
   // destroyed.
-  char* initial_block = nullptr;
+  char* initial_block = kNull;
 
   // The size of the initial block, if provided.
   size_t initial_block_size = 0;

whoisvaska/protobuf_3_12_x

src/google/protobuf/arena.h
@@ -110,6 +110,8 @@ void arena_delete_object(void* object) {
 }
 }  // namespace internal
 
+constexpr char* kNullptr4 = nullptr;
+
 // ArenaOptions provides optional additional parameters to arena construction
 // that control its block-allocation behavior.
 struct ArenaOptions {
@@ -127,7 +129,7 @@ struct ArenaOptions {
   // provided, the block must live at least as long as the arena itself. The
   // creator of the Arena retains ownership of the block after the Arena is
   // destroyed.
-  char* initial_block = nullptr;
+  char* initial_block = kNullptr4;
 
   // The size of the initial block, if provided.
   size_t initial_block_size = 0;